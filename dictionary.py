a633b49c56596c6ce3bd7094577540346757c4a8
xiaodu3096 <309625528@qq.com>
2018-03-11 23:43:45 +0800

    Python字典的用法

--- dictionary.py
+++ dictionary.py
@@ -5,3 +5,16 @@
 # @Site    : 
 # @File    : dictionary.py
 # @Software: PyCharm
+
+favorite_languages = {
+    'jen':'python',
+    'sear':'c',
+    'edward':'ruby'
+}
+
+friends = ['phil','sear']
+
+for name in favorite_languages:
+
+    if name in friends:
+        print('Hi \n'+name.title()+'! I see your favorate languages is '+favorite_languages[name].title())
